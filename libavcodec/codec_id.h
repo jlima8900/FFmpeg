b699dcd6f4b4b5508ebaea7961b01cfeff6b0cd7
jlima8900 <jlima8900@hotmail.com>
2026-04-02 11:32:14 +0100

    libavcodec/codec_id: fix grammar in comment
    Fix 'a MPEG-4' -> 'an MPEG-4'.

--- libavcodec/codec_id.h
+++ libavcodec/codec_id.h
@@ -620,7 +620,7 @@ enum AVCodecID {
 
     AV_CODEC_ID_MPEG2TS = 0x20000, /**< _FAKE_ codec to indicate a raw MPEG-2 TS
                                 * stream (only used by libavformat) */
-    AV_CODEC_ID_MPEG4SYSTEMS = 0x20001, /**< _FAKE_ codec to indicate a MPEG-4 Systems
+    AV_CODEC_ID_MPEG4SYSTEMS = 0x20001, /**< _FAKE_ codec to indicate an MPEG-4 Systems
                                 * stream (only used by libavformat) */
     AV_CODEC_ID_FFMETADATA = 0x21000,   ///< Dummy codec for streams containing only metadata information.
     AV_CODEC_ID_WRAPPED_AVFRAME = 0x21001, ///< Passthrough codec, AVFrames wrapped in AVPacket
